fb64081c1087a43cc95190ca9ab7d6be4b576e40
Lorenzo Ziosi <lorenzoziosi16@gmail.com>
2024-09-24 23:21:57 +0200

    Updated tester

--- _example/tester.sh
+++ _example/tester.sh
@@ -1,14 +1,14 @@
-!/bin/bash
+#!/bin/bash
 
 # Define the URL you want to query
 URL="127.0.0.1:30107"
 
 # Loop indefinitely
-while true; do
+for i in {0..4}; do
     curl -w "\
 time_total:       %{time_total}s\n" \
 -o /dev/null "$URL"
 
     # Wait for 30 seconds
-    sleep 20
+    sleep 1
 done
